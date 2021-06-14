d583ecc0d53154ab50fe99ffcaabdb99c1cb18b9
Kartik <kartikth40@gmail.com>
2021-06-14 10:48:23 +0530

    added Header UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,33 @@ function Header() {
     <div>
       <Nav>
         <Logo src="/images/logo.svg" />
-        <NavMenu></NavMenu>
+        <NavMenu>
+          <a>
+            <img src="/images/home-icon.svg" />
+            <span>HOME</span>
+          </a>
+          <a>
+            <img src="/images/search-icon.svg" />
+            <span>SEARCH</span>
+          </a>
+          <a>
+            <img src="/images/watchlist-icon.svg" />
+            <span>WATHLIST</span>
+          </a>
+          <a>
+            <img src="/images/original-icon.svg" />
+            <span>ORIGINAL</span>
+          </a>
+          <a>
+            <img src="/images/movie-icon.svg" />
+            <span>MOVIES</span>
+          </a>
+          <a>
+            <img src="/images/series-icon.svg" />
+            <span>SERIES</span>
+          </a>
+        </NavMenu>
+        <UserImg src="/images/pp-icon.svg" />
       </Nav>
     </div>
   )
@@ -19,10 +45,57 @@ const Nav = styled.nav`
   height: 70px;
   background-color: #090b13;
   display: flex;
-  align-item: center;
+  align-items: center;
   padding: 0 36px;
 `
 const Logo = styled.img`
   width: 80px;
 `
-const NavMenu = styled.div``
+const NavMenu = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.4px;
+      position: relative;
+
+      &:after {
+        content: '';
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        transform-origin: left;
+        transform: scaleX(0);
+      }
+    }
+    &:hover {
+      span:after {
+        transition: 0.2s;
+        transform: scaleX(1);
+      }
+    }
+  }
+`
+const UserImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  padding: 2px;
+  border: white 2px solid;
+  cursor: pointer;
+`

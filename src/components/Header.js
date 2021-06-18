22c916c12623650479079dd88ab2744e81d1c0c1
Kartik <kartikth40@gmail.com>
2021-06-18 16:08:38 +0530

    added user login with google's firebase auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { auth, provider } from '../firebase'
+import { selectUserName, selectUserPhoto, setSignOut, setUserLogin } from '../features/user/userSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 function Header() {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const userName = useSelector(selectUserName)
+  const userPhoto = useSelector(selectUserPhoto)
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(
+          setUserLogin({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+          })
+        )
+        history.push('/')
+      }
+    })
+  }, [])
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then(({ user }) => {
+      dispatch(
+        setUserLogin({
+          name: user.displayName,
+          email: user.email,
+          photo: user.photoURL,
+        })
+      )
+      history.push('/')
+    })
+  }
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(setSignOut())
+      history.push('/login')
+    })
+  }
+
   return (
     <div>
       <Nav>
         <Logo src="/images/logo.svg" alt="Disney Logo" />
-        <NavMenu>
-          <button>
-            <img src="/images/home-icon.svg" alt="Home icon" />
-            <span>HOME</span>
-          </button>
-          <button>
-            <img src="/images/search-icon.svg" alt="Search icon" />
-            <span>SEARCH</span>
-          </button>
-          <button>
-            <img src="/images/watchlist-icon.svg" alt="Watchlist icon" />
-            <span>WATHLIST</span>
-          </button>
-          <button>
-            <img src="/images/original-icon.svg" alt="Original icon" />
-            <span>ORIGINAL</span>
-          </button>
-          <button>
-            <img src="/images/movie-icon.svg" alt="Movie icon" />
-            <span>MOVIES</span>
-          </button>
-          <button>
-            <img src="/images/series-icon.svg" alt="Series icon" />
-            <span>SERIES</span>
-          </button>
-        </NavMenu>
-        <UserImg src="/images/pp-icon.svg" alt="Profile pic" />
+        {!userName ? (
+          <LoginCotainer>
+            <Login onClick={signIn}>Login</Login>
+          </LoginCotainer>
+        ) : (
+          <>
+            <NavMenu>
+              <button>
+                <img src="/images/home-icon.svg" alt="Home icon" />
+                <span>HOME</span>
+              </button>
+              <button>
+                <img src="/images/search-icon.svg" alt="Search icon" />
+                <span>SEARCH</span>
+              </button>
+              <button>
+                <img src="/images/watchlist-icon.svg" alt="Watchlist icon" />
+                <span>WATHLIST</span>
+              </button>
+              <button>
+                <img src="/images/original-icon.svg" alt="Original icon" />
+                <span>ORIGINAL</span>
+              </button>
+              <button>
+                <img src="/images/movie-icon.svg" alt="Movie icon" />
+                <span>MOVIES</span>
+              </button>
+              <button>
+                <img src="/images/series-icon.svg" alt="Series icon" />
+                <span>SERIES</span>
+              </button>
+            </NavMenu>
+            <UserImg onClick={signOut} src={userPhoto} alt="Profile pic" />
+          </>
+        )}
       </Nav>
     </div>
   )
@@ -107,4 +159,28 @@ const UserImg = styled.img`
   padding: 2px;
   border: white 2px solid;
   cursor: pointer;
+`
+
+const LoginCotainer = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  justify-content: flex-end;
+`
+
+const Login = styled.button`
+  outline: none;
+  border: 2px white solid;
+  background: transparent;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  transition: all 250ms;
+  cursor: pointer;
+
+  &:hover {
+    background: white;
+    color: black;
+  }
 `
